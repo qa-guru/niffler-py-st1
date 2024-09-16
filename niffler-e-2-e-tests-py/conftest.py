80cfbf4a9203d67dfbf20097930827fa6a9e0539
ramzi_bullet <pavel.g@crtex.io>
2024-09-16 21:58:28 +0300

    fix fixture

--- niffler-e-2-e-tests-py/conftest.py
+++ niffler-e-2-e-tests-py/conftest.py
@@ -1,12 +1,11 @@
 import pytest
 from selene.support.shared import browser as selene_browser
-from pageObject.niffler import Niff  # Импортируем класс Niff
+from pageObject.niffler import Niff
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture
 def niff():
     selene_browser.config.base_url = "http://frontend.niffler.dc"
-
     niff = Niff(selene_browser)
-
     yield niff
+    selene_browser.quit()
