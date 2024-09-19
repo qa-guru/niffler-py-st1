c9fbc03731868a8acdb3f6ece6782f28cd2f9832
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2024-09-19 22:44:28 +0300

    rest api advanced part 2.5

--- niffler-e-2-e-tests-python/databases/spend_db.py
+++ niffler-e-2-e-tests-python/databases/spend_db.py
@@ -1,28 +1,19 @@
 from typing import Sequence
 
-import allure
-from allure_commons.types import AttachmentType
 from sqlalchemy import create_engine, Engine, event
 from sqlmodel import Session, select
 
 from models.category import Category
 from models.config import Envs
+from utils.allure_helpers import attach_sql
 
 
 class SpendDb:
-
     engine: Engine
 
     def __init__(self, envs: Envs) -> object:
         self.engine = create_engine(envs.spend_db_url)
-        event.listen(self.engine, "do_execute", fn=self.attach_sql)
-
-    @staticmethod
-    def attach_sql(cursor, statement, parameters, context):
-        # todo вынести в хелпер
-        statement_with_params = statement % parameters
-        name = statement.split(" ")[0] + " " + context.engine.url.database
-        allure.attach(statement_with_params, name=name, attachment_type=AttachmentType.TEXT)
+        event.listen(self.engine, "do_execute", fn=attach_sql)
 
     def get_user_categories(self, username: str) -> Sequence[Category]:
         with Session(self.engine) as session:
--- niffler-e-2-e-tests-python/utils/sessions.py
+++ niffler-e-2-e-tests-python/utils/sessions.py
@@ -1,73 +1,32 @@
-import json
-import logging
-from json import JSONDecodeError
 from urllib.parse import parse_qs, urlparse
 
-import allure
-import curlify
 import requests
-from allure_commons.types import AttachmentType
-from requests import Session, Response
+from requests import Session
+
+from utils.allure_helpers import allure_attach_request
 
 
-def raise_for_status(response: requests.Response):
-    try:
-        response.raise_for_status()
-    except requests.HTTPError as e:
-        if response.status_code == 400:
-            e.add_note(response.text)
-            raise
-
-
-def allure_attach_request(function):
-    """Декоратор логироваания запроса, хедеров запроса, хедеров ответа в allure шаг и аллюр аттачмент и в консоль."""
+def raise_for_status(function):
     def wrapper(*args, **kwargs):
-        method, url = args[1], args[2]
-        with allure.step(f"{method} {url}"):
-
-            response: Response = function(*args, **kwargs)
-
-            curl = curlify.to_curl(response.request)
-            logging.debug(curl)
-            logging.debug(response.text)
-
-            allure.attach(
-                body=curlify.to_curl(curl).encode("utf8"),
-                name=f"Request {response.status_code}",
-                attachment_type=AttachmentType.TEXT,
-                extension=".txt"
-            )
-            try:
-                allure.attach(
-                    body=json.dumps(response.json(), indent=4).encode("utf8"),
-                    name=f"Response json {response.status_code}",
-                    attachment_type=AttachmentType.JSON,
-                    extension=".json"
-                )
-            except JSONDecodeError:
-                allure.attach(
-                    body=response.text.encode("utf8"),
-                    name=f"Response text {response.status_code}",
-                    attachment_type=AttachmentType.TEXT,
-                    extension=".txt")
-            allure.attach(
-                body=json.dumps(response.headers, indent=4).encode("utf8"),
-                name=f"Response headers {response.status_code}",
-                attachment_type=AttachmentType.JSON,
-                extension=".json"
-            )
-        raise_for_status(response)
+        response = function(*args, **kwargs)
+        try:
+            response.raise_for_status()
+        except requests.HTTPError as e:
+            if response.status_code == 400:
+                e.add_note(response.text)
+                raise
         return response
-
     return wrapper
 
 
 class BaseSession(Session):
     """Сессия с прокидыванием base_url и логированием запроса, ответа, хедеров ответа."""
+
     def __init__(self, *args, **kwargs):
         super().__init__()
         self.base_url = kwargs.pop("base_url", "")
 
+    @raise_for_status
     @allure_attach_request
     def request(self, method, url, **kwargs):
         """Логирование запроса и вклейка base_url."""
@@ -77,6 +36,7 @@ class BaseSession(Session):
 class AuthSession(Session):
     """Сессия с прокидыванием base_url и логированием запроса, ответа, хедеров ответа.
     + Автосохранение cookies внутри сессии из кааждого response и redirect response, и 'code'."""
+
     def __init__(self, *args, **kwargs):
         """
         Прокидываем base_url - url авторизации из энвов
@@ -85,6 +45,7 @@ class AuthSession(Session):
         self.base_url = kwargs.pop("base_url", "")
         self.code = None
 
+    @raise_for_status
     @allure_attach_request
     def request(self, method, url, **kwargs):
         """Сохраняем все cookies из redirect'a и сохраняем code авторизации из redirect_uri,
